a1013b79b30cda88eaf7eef60d7ee3eabd853882
tajon1030 <da33202@gmail.com>
2023-12-21 00:59:32 +0900

    section1 Spring Boot로 개발하는 RESTful Service

---
+++ src/main/java/com/example/restfulservice/bean/HelloWorldBean.java
@@ -0,0 +1,11 @@
+package com.example.restfulservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+
+    private final String message;
+}
--- src/main/java/com/example/restfulservice/controller/HelloJavaController.java
+++ src/main/java/com/example/restfulservice/controller/HelloJavaController.java
@@ -1,6 +1,8 @@
 package com.example.restfulservice.controller;
 
+import com.example.restfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,15 @@ public class HelloJavaController {
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "HelloWorld";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("HelloWorld");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("HelloWord, %s", name));
     }
 }
