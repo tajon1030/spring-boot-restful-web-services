06b86ac8130bb6374d0896a507724e33d50a710d
tajon1030 <da33202@gmail.com>
2024-01-09 00:34:40 +0900

    section3 다국어 처리를 위한 Internationalization 구현 방법

--- src/main/java/com/example/restfulservice/RestfulServiceApplication.java
+++ src/main/java/com/example/restfulservice/RestfulServiceApplication.java
@@ -3,6 +3,11 @@ package com.example.restfulservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulServiceApplication {
@@ -14,5 +19,12 @@ public class RestfulServiceApplication {
 //		for(String beanName:allBeanNames){
 //			System.out.println(beanName);
 //		}
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 }
--- src/main/java/com/example/restfulservice/controller/HelloJavaController.java
+++ src/main/java/com/example/restfulservice/controller/HelloJavaController.java
@@ -1,25 +1,43 @@
 package com.example.restfulservice.controller;
 
 import com.example.restfulservice.bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloJavaController {
 
+    private MessageSource messageSource;
+
+    public HelloJavaController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "HelloWorld";
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("HelloWorld");
     }
 
     @GetMapping("/hello-world-bean/path-variable/{name}")
-    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("HelloWord, %s", name));
     }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale
+    ) {
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
+
 }
