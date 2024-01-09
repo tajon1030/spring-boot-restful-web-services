8f1284aff36b02a329d242d389c09695945b45e4
tajon1030 <da33202@gmail.com>
2024-01-09 22:02:03 +0900

    section3 URI를 이용한 버전관리

--- src/main/java/com/example/restfulservice/controller/AdminUserRestController.java
+++ src/main/java/com/example/restfulservice/controller/AdminUserRestController.java
@@ -1,6 +1,7 @@
 package com.example.restfulservice.controller;
 
 import com.example.restfulservice.bean.AdminUser;
+import com.example.restfulservice.bean.AdminUserV2;
 import com.example.restfulservice.bean.User;
 import com.example.restfulservice.dao.UserDaoService;
 import com.example.restfulservice.exception.UserNotFoundException;
@@ -28,7 +29,7 @@ public class AdminUserRestController {
         this.userDaoService = userDaoService;
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/v1/users/{id}")
     public MappingJacksonValue retriveUser4Admin(@PathVariable int id){
         User user = userDaoService.findOne(id);
 
@@ -41,6 +42,28 @@ public class AdminUserRestController {
 
         SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+
+    @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retriveUser4AdminV2(@PathVariable int id){
+        User user = userDaoService.findOne(id);
+
+        AdminUserV2 adminUser = new AdminUserV2();
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }else{
+            BeanUtils.copyProperties(user,adminUser);
+            adminUser.setGrade("VIP");
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "grade");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
         mapping.setFilters(filters);
